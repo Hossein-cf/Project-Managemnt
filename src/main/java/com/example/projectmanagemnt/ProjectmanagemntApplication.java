264aa46e8d3b09ece405151824f7090d60b3205b
Hossein-cf <shakryhsyn1@gmail.com>
2020-11-26 22:26:23 +0330

    uploaded the project

---
+++ src/main/java/com/example/projectmanagemnt/ProjectmanagemntApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagemnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmanagemntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectmanagemntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectmanagemnt/ProjectmanagemntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagemnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagemntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
