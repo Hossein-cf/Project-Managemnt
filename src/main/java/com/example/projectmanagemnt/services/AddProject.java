38ab0ae13936a95dc4a861262810cfd48e28b7b9
aliIsazadeh <isazadhali@gmail.com>
2020-12-05 00:19:05 +0330

    Add Add project first commit.

--- src/main/java/com/example/projectmanagemnt/services/AddProject.java
+++ src/main/java/com/example/projectmanagemnt/services/AddProject.java
@@ -1,4 +1,38 @@
 package com.example.projectmanagemnt.services;
 
+import com.example.projectmanagemnt.DBHelper.DB;
+
+import com.example.projectmanagemnt.models.company.Project;
+import org.springframework.web.bind.annotation.*;
+
 public class AddProject {
+    DB dbHelper = new DB();
+    @PostMapping(path = "/AddProjectPost",consumes = "application/json",produces = "application/json")
+    public void add(@RequestBody Project project){
+        System.out.println(project.getId());
+        dbHelper.addProject(project);
+    }
+
+//    @GetMapping(path = "/AddProjectGetProjectByCustomerId",consumes = "application/json",produces = "application/json")
+//    public Project get(@RequestBody long ID){
+//        System.out.println(ID);
+//        return dbHelper.getProjectByCustomerId(ID);
+//    }
+//
+//    @GetMapping(path = "/AddProjectGetProjectForCompany",consumes = "application/json",produces = "application/json")
+//    public Project get(@RequestBody long ID){
+//        System.out.println(ID);
+//        return dbHelper.getProjectForCompany(ID);
+//    }
+
+    @DeleteMapping(path = "/AddProjectDelete",consumes = "application/json",produces = "application/json")
+    public void delete(@RequestBody Project project){
+        System.out.println(project.getId());
+        dbHelper.deleteProject(project);
+    }
+    @PutMapping(path = "/AddProjectPut",consumes = "application/json",produces = "application/json")
+    public void update(@RequestBody Project project){
+        System.out.println(project.getId());
+        dbHelper.upDateProject(project);
+    }
 }
